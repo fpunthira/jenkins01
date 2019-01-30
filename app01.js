31d585f28589dfc4b4f2e99c0dae4e8bc525557a
root <root@localhost.localdomain>
2019-01-30 02:21:21 -0500

    5 commit

--- app01.js
+++ app01.js
@@ -2,9 +2,9 @@ const express = require('express')
 var app = express()
 const DB = require('./hello')
 
-let DB01 = new DB()
+//let DB01 = new DB()
 
-DB01.getData(DB01.pool)
+//DB01.getData(DB01.pool)
 
 app.get('/', (req,res) => {
     res.send('Hello Express')
